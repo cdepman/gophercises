ab9d2706aa6deca037eab3c064a4e9958abf55ea
Charlie Depman <cdepman@gmail.com>
2020-01-06 16:45:24 -0800

    attempt timer solution

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -8,9 +8,11 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const QuestionsCSVPath = "./problems.csv"
+const TimeAllowanceSeconds = 10
 
 type Score struct {
 	Correct    int
@@ -25,22 +27,38 @@ type QuestionAndAnswer struct {
 func main() {
 	inputReader := bufio.NewReader(os.Stdin)
 	score := Score{}
+	timer := time.NewTimer(time.Second * TimeAllowanceSeconds)
 	list := getQuestionAndAnswerList(QuestionsCSVPath)
+	questionsChannel := make(chan QuestionAndAnswer, len(list))
 	for _, questionAndAnswer := range list {
-		fmt.Printf("What is %s?\n> ", questionAndAnswer.Question)
-		answer, _ := inputReader.ReadString('\n')
-		answer = strings.TrimSuffix(answer, "\n")
-		answerInt, err := strconv.Atoi(answer)
-		if err != nil {
-			fmt.Printf("That's not an integer :(, %s", err)
-		}
-		if answerInt == questionAndAnswer.Answer {
-			score.Correct += 1
-		} else {
-			score.Incorrrect += 1
+		questionsChannel <- questionAndAnswer
+	}
+	for {
+		select {
+		case gameOver := <-timer.C:
+			fmt.Println("Game over!\n")
+			fmt.Printf("You scored %d out of %d at %s", score.Correct, len(list), gameOver)
+			return
+		case nextQuestion := <-questionsChannel:
+			fmt.Printf("What is %s?\n> ", nextQuestion.Question)
+			answer, _ := inputReader.ReadString('\n')
+			answer = strings.TrimSuffix(answer, "\n")
+			answerInt, err := strconv.Atoi(answer)
+			if err != nil {
+				fmt.Printf("That's not an integer :(")
+			}
+			if answerInt == nextQuestion.Answer {
+				score.Correct += 1
+				fmt.Println("Correct!")
+			} else {
+				score.Incorrrect += 1
+				fmt.Println("Incorrect!")
+			}
+		default:
+			fmt.Printf("You scored %d out of %d", score.Correct, len(list))
+			return
 		}
 	}
-	fmt.Printf("Correct: %d\nIncorrrect: %d\n", score.Correct, score.Incorrrect)
 }
 
 func getQuestionAndAnswerList(filePath string) []QuestionAndAnswer {
